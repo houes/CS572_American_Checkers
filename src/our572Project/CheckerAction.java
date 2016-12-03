32748eff0d1787a2d465e9360e186c5c55b47aba
clarionet <Xiaoqian@xqmu.student.iastate.edu>
2016-12-02 19:14:07 -0600

    change of action

--- src/our572Project/CheckerAction.java
+++ src/our572Project/CheckerAction.java
@@ -1,13 +1,22 @@
 package our572Project;
 
+import java.util.List;
+
 public class CheckerAction {
 
 	XYLocation selNode;
 	XYLocation moveTo;
+	List <XYLocation> moveToKingRow;
 
 	public CheckerAction(XYLocation selNode_, XYLocation moveTo_) {
 		selNode = selNode_;
 		moveTo = moveTo_;
+	}
+	
+	public CheckerAction(XYLocation selNode_, XYLocation moveTo_,List<XYLocation> moveToKingRow_) {
+		selNode = selNode_;
+		moveTo = moveTo_;
+		moveToKingRow = moveToKingRow_;
 	}
 
 	public XYLocation getSelNode() {
@@ -18,6 +27,10 @@ public class CheckerAction {
 		return moveTo;
 	}
 
+	public List<XYLocation> getmvoeToKingRow(){
+		return moveToKingRow;
+	}
+	
 	@Override
 	public boolean equals(Object o) {
 		if (null == o || !(o instanceof XYLocation)) {
